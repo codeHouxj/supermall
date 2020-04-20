20c039158b141bdb90b8690f636df269db4e5454
codeHouxj <193380440@qq.comgit config --global user.email>
2020-04-20 15:29:40 +0800

    提交1

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -63,7 +63,12 @@ export default {
   data(){
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods:{
+        'pop':{page:0, list:[]},
+        'news':{page:0, list:[]},
+        'sell':{page:0, list:[]},
+      }
     }
   },
   created(){
