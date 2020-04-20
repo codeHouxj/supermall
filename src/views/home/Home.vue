acc07ea16ba3756798b658bde848c055b8ebd1bf
codeHouxj <193380440@qq.comgit config --global user.email>
2020-04-20 15:27:50 +0800

    首页

---
+++ src/views/home/Home.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"/>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar"
+import HomeSwiper from "./childComps/HomeSwiper"
+import RecommendView from "./childComps/RecommendView"
+import FeatureView from "./childComps/FeatureView"
+
+import TabControl from "components/content/tabControl/TabControl"
+
+import {getHomeMultidata} from "network/home.js"
+
+
+export default {
+  name:'Home',
+  components:{
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl
+  },
+  data(){
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created(){
+    getHomeMultidata().then(res => {
+      this.banners=res.data.banner.list;
+      this.recommends=res.data.recommend.list;
+    })
+  }
+}
+</script>
+
+<style scoped>
+#home {
+  padding-top: 44px;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+  background-color: #fff;
+}
+</style>
